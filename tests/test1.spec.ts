d2dc33555d0c46009e10d75f68379aba7cab0d47
Manuel Rodriguez <manuel98031752@gmail.com>
2026-05-22 11:02:15 -0500

    Updating the project to use a better use case practice, POM model, etc...

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,20 +1,43 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://todomvc.com/examples/react/dist/');
-  await page.getByTestId('text-input').click();
-  await page.getByTestId('text-input').fill('Buy Groceries');
-  await page.getByTestId('text-input').press('Enter');
+test.describe('1. TodoMVC – Tests', () => {
 
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await page.getByTestId('text-input').click();
+    await page.getByTestId('text-input').fill('Buy Groceries');
+    await page.getByTestId('text-input').press('Enter');
+    await page.getByTestId('text-input').fill('Pay bills');
+    await page.getByTestId('text-input').press('Enter');
+    
+  });
+
+test('TC-001 | Add New Todos', async ({ page }) => {
+  
   await expect(page.getByText('Buy Groceries')).toBeVisible();
-
-  await page.getByTestId('text-input').fill('Pay bills');
-  await page.getByTestId('text-input').press('Enter');
-
   await expect(page.getByText('Pay bills')).toBeVisible();
 
   const listItems = page.getByTestId('todo-item');
   await expect(listItems).toHaveCount(2);
   
 //   console.log(`Number of items in the list: ${count}`);
+});
+
+test('TC-002 | Mark Complete', async ({ page }) => {
+
+  const listItems = page.getByTestId('todo-item');
+  await expect(listItems).toHaveCount(2);
+
+  await page.getByTestId('todo-item-toggle').first().click();
+  await page.getByText('Completed', {exact : true}).click();
+  await expect(listItems).toHaveCount(1);
+  await expect(page.locator('.todo-count')).toHaveText('1 item left!')
+
+  //nth() to find elements
+  
+//   console.log(`Number of items in the list: ${count}`);
+});
+
+
+
 });
