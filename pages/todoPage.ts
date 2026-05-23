9efe7d7790a30eb0831cb1fa820a8a775e6c2c9b
Manuel Rodriguez <manuel98031752@gmail.com>
2026-05-22 19:00:33 -0500

    Updated test 1 with the correct assertion and POM

--- pages/todoPage.ts
+++ pages/todoPage.ts
@@ -1,5 +1,52 @@
-import { Page, Locator} from '@playwright/test';
+import { Page, Locator, expect} from '@playwright/test';
 
-export class todoPage {
-    
+export class todoPage { //name has to match the ts file
+
+    //properties
+    private readonly page: Page;
+    private readonly fieldInput: Locator;
+    private readonly todoItem: Locator;
+    private readonly buttonItem: Locator;
+    private readonly listItems: Locator;
+
+    constructor(page:Page) {
+        this.page = page;
+        this.fieldInput = page.getByTestId('text-input');
+        this.todoItem = page.getByTestId('todo-item-label');
+        this.buttonItem = page.getByTestId('todo-item-toggle');
+        this.listItems = page.getByTestId('todo-item');
+    }
+
+    //methods
+    async navigate(): Promise<void>{
+        await this.page.goto('');
+    }
+
+    async addItem(itemName: string): Promise<void>{
+
+        await this.fieldInput.click();
+        await this.fieldInput.fill(itemName);
+        await this.fieldInput.press('Enter');
+
+    }    
+
+    // async listOfItems(numberOfItems: number): Promise<void>{
+
+    //     await expect(this.listItems).toHaveCount(numberOfItems);
+
+    // }
+
+    listOfItems(): Locator{
+
+        return this.listItems;
+
+    }
+
+    getItemFromList(name: string): Locator{
+
+        return this.todoItem.filter({ hasText: name });
+        //page.getByText('Buy Groceries')
+
+    }
+
 }
--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,42 +1,52 @@
 import { test, expect } from '@playwright/test';
+import { todoPage } from '../pages/todoPage';
+
+
 
 test.describe('1. TodoMVC – Tests', () => {
 
+  let openWebsite: todoPage;
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-    await page.getByTestId('text-input').click();
-    await page.getByTestId('text-input').fill('Buy Groceries');
-    await page.getByTestId('text-input').press('Enter');
-    await page.getByTestId('text-input').fill('Pay bills');
-    await page.getByTestId('text-input').press('Enter');
+    openWebsite = new todoPage(page);
+    await openWebsite.navigate();
     
   });
 
 test('TC-001 | Add New Todos', async ({ page }) => {
   
-  await expect(page.getByText('Buy Groceries')).toBeVisible();
-  await expect(page.getByText('Pay bills')).toBeVisible();
+  const expectedTodos = ['Buy Groceries', 'Pay bills'];
 
-  const listItems = page.getByTestId('todo-item');
-  await expect(listItems).toHaveCount(2);
+  for (const todo of expectedTodos){
+    await openWebsite.addItem(todo);
+  }
+  
+  for (const result of expectedTodos){
+    await expect(openWebsite.getItemFromList(result)).toBeVisible();
+  }
+
+  await expect(openWebsite.listOfItems()).toHaveCount(2);
+  // openWebsite.listOfItems(2);
+
+
   
 //   console.log(`Number of items in the list: ${count}`);
 });
 
-test('TC-002 | Mark Complete', async ({ page }) => {
+// test('TC-002 | Mark Complete', async ({ page }) => {
 
-  const listItems = page.getByTestId('todo-item');
-  await expect(listItems).toHaveCount(2);
+//   const listItems = page.getByTestId('todo-item');
+//   await expect(listItems).toHaveCount(2);
 
-  await page.getByTestId('todo-item-toggle').first().click();
-  await page.getByText('Completed', {exact : true}).click();
-  await expect(listItems).toHaveCount(1);
-  await expect(page.locator('.todo-count')).toHaveText('1 item left!')
+//   await page.getByTestId('todo-item-toggle').first().click();
+//   await page.getByText('Completed', {exact : true}).click();
+//   await expect(listItems).toHaveCount(1);
+//   await expect(page.locator('.todo-count')).toHaveText('1 item left!')
 
-  //nth() to find elements
+//   //nth() to find elements
   
-//   console.log(`Number of items in the list: ${count}`);
-});
+// //   console.log(`Number of items in the list: ${count}`);
+// });
 
 
 
